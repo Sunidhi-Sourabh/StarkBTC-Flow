1492b20a1709782dcfe1980ac211403f2adba74c
Sunidhi Sourabh <sunidhi.sourabh@gmail.com>
2025-10-03 13:21:40 +0530

    report_generator.h

---
+++ src/report_generator.h
@@ -0,0 +1,27 @@
+#ifndef REPORT_GENERATOR_H
+#define REPORT_GENERATOR_H
+
+#include <string>
+#include "swap_simulator.h"
+#include "wallet_verifier.h"
+
+namespace StarkBTC {
+
+    class ReportGenerator {
+    public:
+        ReportGenerator(const SwapResult& swap, const WalletStatus& wallet);
+
+        std::string generateMarkdown();
+        std::string generateHTML();
+        std::string generateJSON();
+        void saveToFile(const std::string& content, const std::string& format);
+        void autoOpenHTML(const std::string& filepath);
+
+    private:
+        SwapResult swap_;
+        WalletStatus wallet_;
+    };
+
+}
+
+#endif // REPORT_GENERATOR_H
