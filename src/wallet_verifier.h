f0157638abc2eeb6b212cebd40e1fe9c4d22e735
Sunidhi Sourabh <sunidhi.sourabh@gmail.com>
2025-10-03 13:18:52 +0530

    wallet_verifier.h

--- src/wallet_verifier.h
+++ src/wallet_verifier.h
@@ -1,1 +1,31 @@
+#ifndef WALLET_VERIFIER_H
+#define WALLET_VERIFIER_H
+
+#include <string>
+
+namespace StarkBTC {
+
+    struct WalletStatus {
+        bool is_valid;
+        std::string address;
+        std::string reason;
+        int score;
+    };
+
+    class WalletVerifier {
+    public:
+        WalletVerifier(const std::string& wallet_address);
+
+        WalletStatus verify();
+        int calculateScore(const std::string& wallet_address);
+        std::string generateBadge(int score);
+
+    private:
+        std::string wallet_address_;
+        int score_threshold_;
+    };
+
+}
+
+#endif // WALLET_VERIFIER_H
 
